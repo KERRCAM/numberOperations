4eaab0c7af67cf0edc64ce2c8098f64e5c94cd56
Kerr Cameron <KC210253@reigate.ac.uk>
2021-09-22 10:01:31 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+
+    int[] numbers = new int[5];
+    for (int i = 0; i <5 ; i++) {
+        int num = input.nextInt();
+        numbers[i]=num;
+    }
+    
+
+    }
+}
